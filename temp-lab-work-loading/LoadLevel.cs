5db6dd07a06b483821427f5045843474ae603801
Joseph <josephaohagan@gmail.com>
2017-10-18 16:33:54 +0100

    Temp upload with scene loading v1 work from the lab

---
+++ temp-lab-work-loading/LoadLevel.cs
@@ -0,0 +1,35 @@
+﻿// https://medium.com/fusedvr/implementing-vr-scene-transitions-c27861a9ac77
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+    public int currentLevel = 0;
+    public string[] levelNames= new string[2] { "HelloWorldVR", "example" };
+
+    static LoadLevel s = null;
+
+	// Use this for initialization
+	void Start () {
+		if (s == null)
+        {
+            s = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            currentLevel = (currentLevel + 1) % 2;
+            SteamVR_LoadLevel.Begin(levelNames[currentLevel]);
+        }
+    }
+}
